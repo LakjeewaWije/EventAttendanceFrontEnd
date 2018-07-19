990b51d35b9f213f463a0ef038e05b7c488e837b
ajmal <ajmal@Populos-Mac-mini-3.local>
2018-07-19 11:11:34 +0530

    App component and app module imports organised

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
+// Third Party Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { QrModule } from './qr/qr.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// App Imports
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
-import { HttpClientModule } from '@angular/common/http';
 import { EventModule } from './event/event.module';
-import { FormsModule } from '@angular/forms';
 import { EventComponent } from './event/event/event.component';
 import * as firebase from 'firebase';
+import { QrModule } from './qr/qr.module';
 
 firebase.initializeApp(environment.config);
 
