9019a2579a869fb854927f3d1371c15f859f3048
Lakjeewa <lakjeewa@Populos-Mac-mini-2.local>
2018-07-20 19:03:25 +0530

    returned the create and get events requests to handle responses

--- src/app/event/event.service.ts
+++ src/app/event/event.service.ts
@@ -3,7 +3,9 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
-
+// import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 // App Imports
 import {Event} from './event-model';
 import {environment} from '../../environments/environment';
@@ -27,53 +29,17 @@ export class EventService {
    * @param {string} eventName
    */
   createEvent(eventName: string) {
-    this.event.setEName(eventName);
-
     this.httpHeader;
-    this.http.post<any>(environment.baseUrl + environment.event, {
-      eventName: this.event.getEventName(),
+    return this.http.post<any>(environment.baseUrl + environment.event, {
+      eventName: eventName,
       eventDesc: 'lakiyaaaa event ',
       eventDateTime: this.event.getEventDate()
-    }, {headers: this.httpHeader}).subscribe(res => {
-        console.log('2');
-        const eventsFromResponse = res.data;
-        console.log(eventsFromResponse);
-        this.events.length = 0;
-        for (let i = 0; i < eventsFromResponse.length; i++) {
-          this.events.push(eventsFromResponse[i]);
-        }
-        console.log(this.events);
-        console.log('3');
-      },
-      err => {
-        console.log('Error occurred');
-      }
-    );
+    }, {headers: this.httpHeader});
   }
 
   getEvents() {
-
     this.httpHeader;
-    this.http.get<any>(environment.baseUrl + environment.con, {headers: this.httpHeader}).subscribe(
-      data => {
-        console.log('3');
-        const eventsFromResponse = data.data;
-        // console.log(eventsFromResponse);
-        this.events.length = 0;
-        for (let i = 0; i < eventsFromResponse.length; i++) {
-          this.events.push(eventsFromResponse[i]);
-          console.log('4');
-        }
-        // console.log(this.events);
-      },
-      (err: HttpErrorResponse) => {
-        if (err.error instanceof Error) {
-          console.log('Client-side error occurred.');
-        } else {
-          console.log('Server-side error occurred.');
-        }
-      }
-    );
+    return this.http.get<any>(environment.baseUrl + environment.con, {headers: this.httpHeader});
   }
 
   getFcmToken() {
